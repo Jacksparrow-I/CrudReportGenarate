290816852d9425e5e1c11f21420edee2d0b7d8f7
nprajapati <nprajapati@NISARG-TRAINEE>
2020-11-06 18:39:16 +0530

    Services

---
+++ Business/Customer_Invoice_Payment_Management.BusinessLogic/Services/Abstract/ILoginServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customer_Invoice_Payment_Management.BusinessLogic.Services.Abstract
+{
+    public interface ILoginServices
+    {
+    }
+}
---
+++ Business/Customer_Invoice_Payment_Management.BusinessLogic/Services/Concrete/ReportServices.cs
@@ -0,0 +1,11 @@
+﻿using Customer_Invoice_Payment_Management.BusinessLogic.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customer_Invoice_Payment_Management.BusinessLogic.Services.Concrete
+{
+    public class ReportServices : IReportServices 
+    {
+    }
+}
